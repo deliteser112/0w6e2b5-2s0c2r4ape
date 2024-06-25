8f51df70e2039bad21e6ae5e4da5b9d79f6f44f5
deliteser112 <deliteser112@gmail.com>
2024-06-25 16:54:13 -0400

    Adjust puppeteer virtual browser config

--- src/utils/scraper.ts
+++ src/utils/scraper.ts
@@ -10,8 +10,17 @@ const scrape = async (queryParams: QueryParams) => {
   //Lanuch browser and target page.
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ]
+});
   const page = await browser.newPage();
   await page.goto(SCRAPING_URL, { waitUntil: 'networkidle2' });
 
